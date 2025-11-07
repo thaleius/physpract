0712ec482866f36b0ecb41c2b0e9f45a940468f1
thaleius <58767350+thaleius@users.noreply.github.com>
2025-11-07 18:22:49 +0100

    fix: correct logic in string representation for scientific notation in Value class

--- src/physpract/uncertainties.py
+++ src/physpract/uncertainties.py
@@ -87,9 +87,9 @@ class Value:
   def __str__(self):
     rounded_value, rounded_uncertainty = roundToSignificantFigures(self.value, self.uncertainty)
     tuple = rounded_value.as_tuple()
-    if abs(tuple.exponent) >= 3 and abs(correct) > 1:
+    correct = tuple.exponent+len(tuple.digits)-1
+    if abs(correct) >= 3:
       # uncertainty = ''.join(map(str, rounded_uncertainty.as_tuple().digits))
-      correct = tuple.exponent+len(tuple.digits)-1
       value = rounded_value.scaleb(-correct)
       uncertainty = rounded_uncertainty.scaleb(-correct)
       if uncertainty == 0:
@@ -108,7 +108,7 @@ class Value:
     value = to_non_scientific_string(rounded_value)
     s = ""
     correct = tuple.exponent+len(tuple.digits)-1
-    if abs(tuple.exponent) >= 3 and abs(correct) > 1:
+    if abs(correct) >= 3:
       if rounded_uncertainty == 0:
         s = f"{rounded_value.scaleb(-correct)}e{correct}"
       else:
